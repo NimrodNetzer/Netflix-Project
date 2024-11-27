01e58ad013ea0c002d33a4c6d55a868eda3c02b3
idorozin <rozin.ido@gmail.com>
2024-11-27 20:39:53 +0200

    add main.cpp

---
+++ RecommendationSystem/test/test1.cpp
@@ -0,0 +1,11 @@
+#include "test1.h"
+#include <gtest/gtest.h>
+
+
+TEST(hello, world) {
+    EXPECT_EQ(2, 2);
+}
+
+TEST(hello, world2) {
+    EXPECT_EQ(2, 2);
+}
---
+++ RecommendationSystem/test/test1.h
@@ -0,0 +1,16 @@
+//
+// Created by ido on 11/27/24.
+//
+
+#ifndef TEST1_H
+#define TEST1_H
+
+
+
+class test1 {
+
+};
+
+
+
+#endif //TEST1_H
