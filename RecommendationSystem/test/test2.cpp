831b9f235634806778b07978fee4b8281a267eda
idorozin <rozin.ido@gmail.com>
2024-11-27 20:37:46 +0200

    gtest configuration

---
+++ RecommendationSystem/test/test2.cpp
@@ -0,0 +1,11 @@
+#include "test2.h"
+#include <gtest/gtest.h>
+
+
+TEST(test2, from2) {
+    EXPECT_EQ(2, 2);
+}
+
+TEST(test2, from3) {
+    EXPECT_EQ(1, 2);
+}
---
+++ RecommendationSystem/test/test2.h
@@ -0,0 +1,16 @@
+//
+// Created by ido on 11/27/24.
+//
+
+#ifndef TEST2_H
+#define TEST2_H
+
+
+
+class test2 {
+
+};
+
+
+
+#endif //TEST2_H
