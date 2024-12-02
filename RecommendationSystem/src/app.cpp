114027833970003205cc661fd6f0c15ad16c7d71
Or Liberman <or.liber1@gmail.com>
2024-12-02 16:12:50 +0200

    add app class

---
+++ RecommendationSystem/src/app.cpp
@@ -0,0 +1,17 @@
+//
+// Created by orlib on 02/12/2024.
+//
+
+#include "app.h"
+
+// Declaration of private variables
+std::vector<std::string> users;
+std::vector<std::string> movies;
+
+// Constructor to initialize app
+// Currently no explicit constructor needed as we use default behavior.
+
+
+
+
+
---
+++ RecommendationSystem/src/app.h
@@ -0,0 +1,22 @@
+/*
+// Created by orlib on 02/12/2024.
+*/
+
+#ifndef MY_TEST_APP_H
+#define MY_TEST_APP_H
+
+#include <vector>
+#include <string>
+
+class app {
+private:
+    // Vector to store user identifiers (e.g., usernames or IDs)
+    std::vector<std::string> users;
+    // Vector to store movie identifiers (e.g., titles or IDs)
+    std::vector<std::string> movies;
+
+public:
+
+};
+
+#endif //MY_TEST_APP_H
