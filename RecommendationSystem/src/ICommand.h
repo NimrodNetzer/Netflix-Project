86f3ff06ab9a28a1d73ac34f61209d52cf33c6b8
Or Liberman <or.liber1@gmail.com>
2024-12-02 16:12:32 +0200

    add ICommand interface

---
+++ RecommendationSystem/src/ICommand.h
@@ -0,0 +1,15 @@
+//
+// Created by orlib on 02/12/2024.
+//
+
+#ifndef MY_TEST_ICOMMAND_H
+#define MY_TEST_ICOMMAND_H
+
+
+class ICommand {
+public:
+    virtual void execute() = 0;
+};
+
+
+#endif //MY_TEST_ICOMMAND_H
