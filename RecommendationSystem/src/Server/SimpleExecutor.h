9c84fe21b385bb73519c2b859a15c0a0c88c155c
idorozin <rozin.ido@gmail.com>
2024-12-18 12:57:39 +0200

    Create Interface to work with threads

---
+++ RecommendationSystem/src/Server/SimpleExecutor.h
@@ -0,0 +1,25 @@
+#ifndef SIMPLEEXECUTOR_H
+#define SIMPLEEXECUTOR_H
+#include <thread>
+#include <vector>
+
+#include "Executor.h"
+
+class SimpleExecutor : public Executor {
+public:
+    void execute(Runnable& task) override;
+    ~SimpleExecutor() override {
+        for (std::thread &t : threads) {
+            if (t.joinable()) {
+                t.join();
+            }
+        }
+    }
+
+private:
+    std::vector<std::thread> threads;
+    std::mutex threads_mutex; // Protects the threads vector
+};
+
+
+#endif //SIMPLEEXECUTOR_H
