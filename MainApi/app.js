41003b8029f8184fbe735f980d8f9c0a5acbf0f4
idorozin <rozin.ido@gmail.com>
2024-12-31 14:29:26 +0200

    intialize project

---
+++ MainApi/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('custom-env').env(process.env.NODE_ENV,'./config');
+mongoose.connect(process.env.CONNECTION_STRING)
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json());
+app.listen(process.env.PORT);
