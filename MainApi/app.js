0102d46467ef5561b485de98c65d312f0cc29f77
idorozin <rozin.ido@gmail.com>
2025-01-06 16:43:02 +0200

    update

--- MainApi/app.js
+++ MainApi/app.js
@@ -6,6 +6,7 @@ const cors = require('cors'); // Middleware for enabling Cross-Origin Resource S
 const mongoose = require('mongoose'); // Library for interacting with MongoDB
 const movieRoutes = require('./routes/movie');
 const categoryRoutes = require('./routes/category');
+const searchRoutes = require('./routes/search');
 
 // Import route files
 const users = require('./routes/user'); // Routes for user-related operations
@@ -34,6 +35,7 @@ app.use('/api/users', users); // Routes for user operations (e.g., create, updat
 app.use('/api/tokens', tokens); // Routes for token operations (e.g., validate, create tokens)
 app.use('/api/movies', movieRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/movies/search', searchRoutes)
 // Start the server and listen on the port specified in environment variables
 const PORT = process.env.PORT || 3000; // Default to port 3000 if not specified
 app.listen(PORT, () => {
