461c505a2c37372ed1ee4635f43e17d9473b8b10
orliber <or.liber1@gmail.com>
2025-01-22 19:41:30 +0200

    changes to be committed

--- netflix-web/src/components/Signup.js
+++ netflix-web/src/components/Signup.js
@@ -8,28 +8,32 @@ import profile3 from '../assets/profile3.webp';
 const Signup = () => {
     const location = useLocation();
     const navigate = useNavigate();
-    const [selectedPicture, setSelectedPicture] = useState(null); // State for selected profile picture
+    const [picture, setSelectedPicture] = useState(''); // State for selected profile picture
 
     const [email, setEmail] = useState(location.state?.email || ''); // Initialize email with passed state or empty string
     const [password, setPassword] = useState('');
     const [nickname, setNickname] = useState('');
-    const [picture, setPicture] = useState(null);
     const [errorMessage, setErrorMessage] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
     const handleSignup = async (event) => {
         event.preventDefault();
+
+        if (!picture) {
+            alert('Please select a profile picture.');
+            return;
+        }
+
         setIsLoading(true);
 
-        
         try {
             const response = await fetch('http://localhost:4000/api/users', {
                 method: 'POST',
                 headers: {
-                  'Content-Type': 'application/json',
+                    'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email, password, nickname , picture }),
-              });
+                body: JSON.stringify({ email, password, nickname, picture }),
+            });
 
             if (response.ok) {
                 alert('Signup successful! You can now log in.');
@@ -46,63 +50,63 @@ const Signup = () => {
     };
 
     return (
-      <div className="signup-container">
-          <h2>Sign Up</h2>
-          <form onSubmit={handleSignup}>
-              {/* Email Field */}
-              <div className="form-group">
-                  <label htmlFor="email">Email:</label>
-                  <input
-                      type="email"
-                      id="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Enter your email"
-                      required
-                      pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
-                      title="Please enter a valid email address in the format: example@domain.com"
-                  />
-              </div>
+        <div className="signup-container">
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSignup}>
+                {/* Email Field */}
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email"
+                        required
+                        pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
+                        title="Please enter a valid email address in the format: example@domain.com"
+                    />
+                </div>
 
-              {/* Password Field */}
-              <div className="form-group">
-                  <label htmlFor="password">Password:</label>
-                  <input
-                      type="password"
-                      id="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder="Enter your password"
-                      required
-                      pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-                      title="Password must be at least 8 characters long, contain at least one letter and one number, and use only English letters and numbers."
-                  />
-              </div>
+                {/* Password Field */}
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter your password"
+                        required
+                        pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+                        title="Password must be at least 8 characters long, contain at least one letter and one number, and use only English letters and numbers."
+                    />
+                </div>
 
-              {/* Nickname Field */}
-              <div className="form-group">
-                  <label htmlFor="nickname">Nickname:</label>
-                  <input
-                      type="text"
-                      id="nickname"
-                      value={nickname}
-                      onChange={(e) => setNickname(e.target.value)}
-                      placeholder="Enter your nickname"
-                      required
-                      pattern="^[A-Za-z\d_]{3,}$"
-                      title="Nickname must be at least 3 characters long and can include letters, numbers, and underscores only."
-                  />
-              </div>
+                {/* Nickname Field */}
+                <div className="form-group">
+                    <label htmlFor="nickname">Nickname:</label>
+                    <input
+                        type="text"
+                        id="nickname"
+                        value={nickname}
+                        onChange={(e) => setNickname(e.target.value)}
+                        placeholder="Enter your nickname"
+                        required
+                        pattern="^[A-Za-z\d_]{3,}$"
+                        title="Nickname must be at least 3 characters long and can include letters, numbers, and underscores only."
+                    />
+                </div>
 
-              {/* Profile Picture Selection */}
-              <div className="form-group">
-                    <label>Choose a Profile Picture:</label>
+                {/* Profile Picture Selection */}
+                <div className="form-group">
+                    <label htmlFor="profile-picture">Choose a Profile Picture:</label>
                     <div className="profile-picture-options">
                         {/* Option 1 */}
                         <img
                             src={profile1}
                             alt="Profile 1"
-                            className={`profile-thumbnail ${selectedPicture === 'profile1' ? 'selected' : ''}`}
+                            className={`profile-thumbnail ${picture === 'profile1' ? 'selected' : ''}`}
                             onClick={() => setSelectedPicture('profile1')}
                         />
 
@@ -110,7 +114,7 @@ const Signup = () => {
                         <img
                             src={profile2}
                             alt="Profile 2"
-                            className={`profile-thumbnail ${selectedPicture === 'profile2' ? 'selected' : ''}`}
+                            className={`profile-thumbnail ${picture === 'profile2' ? 'selected' : ''}`}
                             onClick={() => setSelectedPicture('profile2')}
                         />
 
@@ -118,27 +122,27 @@ const Signup = () => {
                         <img
                             src={profile3}
                             alt="Profile 3"
-                            className={`profile-thumbnail ${selectedPicture === 'profile3' ? 'selected' : ''}`}
+                            className={`profile-thumbnail ${picture === 'profile3' ? 'selected' : ''}`}
                             onClick={() => setSelectedPicture('profile3')}
                         />
                     </div>
                 </div>
 
-              {/* Error Message */}
-              {errorMessage && <p className="error-message">{errorMessage}</p>}
+                {/* Error Message */}
+                {errorMessage && <p className="error-message centered">{errorMessage}</p>}
 
-              {/* Signup Button */}
-              <button type="submit" disabled={isLoading}>
-                  {isLoading ? 'Signing up...' : 'Signup'}
-              </button>
+                {/* Signup Button */}
+                <button type="submit" disabled={isLoading}>
+                    {isLoading ? 'Signing up...' : 'Signup'}
+                </button>
 
-              {/* Additional Options */}
-              <div className="additional-options">
-                  <a onClick={() => navigate('/login')}>Already have an account? Sign in</a>
-              </div>
-          </form>
-      </div>
-  );
+                {/* Additional Options */}
+                <div className="additional-options">
+                    <a onClick={() => navigate('/login')}>Already have an account? Sign in</a>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Signup;
