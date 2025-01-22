db79816b69cf4c4f5b0d2d02ef6f1ff3671d6f73
idorozin <rozin.ido@gmail.com>
2025-01-22 20:15:46 +0200

    Clickable logo

---
+++ netflix-web/src/components/ClickableLogo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/LOGO.jpg';// Replace with your actual logo path
+
+const ClickableLogo = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <img
+      src={logo}
+      alt="Logo"
+      className="logo"
+      onClick={handleClick}
+      style={{ cursor: 'pointer' }}
+    />
+  );
+};
+
+export default ClickableLogo;
