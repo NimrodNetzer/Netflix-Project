c62ff56346db28336498d9d010a0bbe5ddbd4ea6
NimrodNetzer <nimrodnetzer@gmail.com>
2025-01-22 11:37:53 +0200

    push

---
+++ netflix-web/src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <h1>Unlimited movies, TV shows, and more.</h1>
+                <h2>Watch anywhere. Cancel anytime.</h2>
+                <p>Ready to watch? Enter your email to create or restart your membership.</p>
+                <div className="input-container">
+                    <input type="email" placeholder="Email address" />
+                    <button>Get Started &gt;</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
