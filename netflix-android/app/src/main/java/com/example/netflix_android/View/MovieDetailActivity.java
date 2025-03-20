39f6cfc01ab9d41df04304c6fff0b4e608c1e85a
orliber <or.liber1@gmail.com>
2025-03-20 14:01:45 +0200

    merge

--- netflix-android/app/src/main/java/com/example/netflix_android/View/MovieDetailActivity.java
+++ netflix-android/app/src/main/java/com/example/netflix_android/View/MovieDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.netflix_android.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
