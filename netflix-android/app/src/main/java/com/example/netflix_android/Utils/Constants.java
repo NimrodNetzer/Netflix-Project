78545bc9c390911dfe01993c500eaadc5bfa3f46
idorozin <rozin.ido@gmail.com>
2025-04-04 23:18:15 +0300

    port

--- netflix-android/app/src/main/java/com/example/netflix_android/Utils/Constants.java
+++ netflix-android/app/src/main/java/com/example/netflix_android/Utils/Constants.java
@@ -1,5 +1,5 @@
 package com.example.netflix_android.Utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.1.40:4000/";
+    public static final String BASE_URL = "http://192.168.1.40:3000/";
 }
